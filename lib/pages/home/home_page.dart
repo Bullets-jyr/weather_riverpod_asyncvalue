13ef478252c9fc3fb0b7c7b803a94ada6d582ed6
Bullets-jyr <jyr@bullets.co.kr>
2024-02-06 16:15:13 +0900

    150. ThemeState, themeProvider

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,11 +4,15 @@ import 'package:weather_riverpod_asyncvalue/extensions/async_value_xx.dart';
 import 'package:weather_riverpod_asyncvalue/pages/home/providers/weather_provider.dart';
 import 'package:weather_riverpod_asyncvalue/repositories/providers/weather_repository_provider.dart';
 
+import '../../constants/constants.dart';
+import '../../models/current_weather/app_weather.dart';
 import '../../models/current_weather/current_weather.dart';
 import '../../models/custom_error/custom_error.dart';
 import '../../widgets/error_dialog.dart';
 import '../search/search_page.dart';
 import '../temp_settings/temp_settings_page.dart';
+import 'providers/theme_provider.dart';
+import 'providers/theme_state.dart';
 import 'widgets/show_weather.dart';
 
 class HomePage extends ConsumerStatefulWidget {
@@ -36,6 +40,18 @@ class _HomePageState extends ConsumerState<HomePage> {
           (previous, next) {
         // Only Error
         next.whenOrNull(
+          data: (CurrentWeather? currentWeather) {
+            if (currentWeather == null) {
+              return;
+            }
+            final weather = AppWeather.fromCurrentWeather(currentWeather);
+
+            if (weather.temp < kWarmOrNot) {
+              ref.read(themeProvider.notifier).changeTheme(const DarkTheme());
+            } else {
+              ref.read(themeProvider.notifier).changeTheme(const LightTheme());
+            }
+          },
           error: (error, stackTrace) {
             errorDialog(context, (error as CustomError).errMsg);
             // showDialog(
